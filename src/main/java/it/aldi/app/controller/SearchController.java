6c9bba01b48521dd0e02128c3811262454dceba0
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-13 19:16:36 +0700

    Refactor and optimize import

--- src/main/java/it/aldi/app/controller/SearchController.java
+++ src/main/java/it/aldi/app/controller/SearchController.java
@@ -3,13 +3,12 @@ package it.aldi.app.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class SearchController {
 
-    @GetMapping("/search")
+    @GetMapping(Routes.SEARCH)
     public String getSearch() {
         log.info("Showing search result page");
         return "search/search";
