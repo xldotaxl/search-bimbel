d448f0946f71f0e51e6adf29ae0e409bf4a1de4e
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-15 00:57:17 +0700

    Rewrite register page and controller and fix test

--- src/main/java/it/aldi/app/controller/RegisterController.java
+++ src/main/java/it/aldi/app/controller/RegisterController.java
@@ -3,37 +3,77 @@ package it.aldi.app.controller;
 import it.aldi.app.controller.dto.BimbelUserDto;
 import it.aldi.app.domain.BimbelUser;
 import it.aldi.app.service.BimbelUserService;
+import it.aldi.app.util.ErrorMsgConstant;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+import static it.aldi.app.util.ControllerConstant.redirect;
 
 @Slf4j
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RegisterController {
+
+    private static final String REGISTER_VIEW = "register/register";
 
     private final @NonNull BimbelUserService bimbelUserService;
 
+    private final @NonNull ErrorMsgConstant errorMsgConstant;
+
     @GetMapping(Routes.REGISTER)
-    public String viewRegisterPage() {
-        return "register/register";
+    public String viewRegisterPage(Model model) {
+        model.addAttribute(new BimbelUserDto());
+        return REGISTER_VIEW;
     }
 
     @PostMapping(Routes.REGISTER)
-    public String postRegister(@ModelAttribute BimbelUserDto bimbelUserDto, BindingResult bindingResult) {
-        log.info("post request coming");
+    public ModelAndView postRegister(@Valid @ModelAttribute BimbelUserDto bimbelUserDto, BindingResult bindingResult) {
+        log.debug("Registering user: {}", bimbelUserDto);
+
         if (bindingResult.hasErrors()) {
-            log.error("There's error occured when user input form");
-            return Routes.REGISTER;
+            return wrongInputModelView(bimbelUserDto);
+        }
+
+        String errorMsg = verifyExistingData(bimbelUserDto);
+        if (!errorMsg.isEmpty()) {
+            return dataExistsModelView(bindingResult, errorMsg);
         }
 
         BimbelUser bimbelUser = BimbelUser.from(bimbelUserDto);
         bimbelUserService.save(bimbelUser);
-        return "redirect:" + Routes.INDEX;
+
+        return new ModelAndView(redirect() + Routes.SIGNIN);
+    }
+
+    private static ModelAndView dataExistsModelView(BindingResult bindingResult, String errorMsg) {
+        log.error("Failed registration, cause: {}", errorMsg);
+        bindingResult.reject(errorMsg);
+        return new ModelAndView(REGISTER_VIEW, "errMsg", errorMsg);
+    }
+
+    private static ModelAndView wrongInputModelView(@ModelAttribute @Valid BimbelUserDto bimbelUserDto) {
+        log.error("There's an error occured when user register");
+        return new ModelAndView(REGISTER_VIEW, "bimbelUserDto", bimbelUserDto);
+    }
+
+    private String verifyExistingData(BimbelUserDto bimbelUserDto) {
+        if (bimbelUserService.findByUsername(bimbelUserDto.getUsername()) != null) {
+            return errorMsgConstant.getEmailExists();
+        }
+        if (bimbelUserService.findByEmail(bimbelUserDto.getEmail()) != null) {
+            return errorMsgConstant.getUsernameExists();
+        }
+        return "";
     }
 }
--- src/test/java/it/aldi/app/controller/RegisterControllerTest.java
+++ src/test/java/it/aldi/app/controller/RegisterControllerTest.java
@@ -2,11 +2,13 @@ package it.aldi.app.controller;
 
 import it.aldi.app.Application;
 import it.aldi.app.service.BimbelUserService;
+import it.aldi.app.util.ErrorMsgConstant;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,13 +27,17 @@ public class RegisterControllerTest {
     @MockBean
     private BimbelUserService bimbelUserService;
 
+    @MockBean
+    private ErrorMsgConstant errorMsgConstant;
+
     @Before
     public void setup() {
-        RegisterController registerController = new RegisterController(bimbelUserService);
-        this.mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
+        RegisterController registerController = new RegisterController(bimbelUserService, errorMsgConstant);
+        mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
     }
 
     @Test
+    @WithAnonymousUser
     public void testRegister() throws Exception {
         mockMvc.perform(get("/register"))
             .andExpect(status().isOk())
