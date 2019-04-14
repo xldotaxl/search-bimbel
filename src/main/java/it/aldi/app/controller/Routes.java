581a04afab64c1e1e578ab4f5e53b24bf818abe5
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-14 21:14:36 +0700

    Add logout routes

--- src/main/java/it/aldi/app/controller/Routes.java
+++ src/main/java/it/aldi/app/controller/Routes.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Routes {
     public static final String INDEX = "/";
+    public static final String LOGOUT = "/logout";
     public static final String REGISTER = "/register";
     public static final String SEARCH = "/search";
     public static final String SIGNIN = "/signin";
