0060d26648c9455494c8365c30ab67c1d90d378b
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-14 21:13:33 +0700

    Add controller constant

---
+++ src/main/java/it/aldi/app/util/ControllerConstant.java
@@ -0,0 +1,12 @@
+package it.aldi.app.util;
+
+public final class ControllerConstant {
+    public static String redirect() {
+        return REDIRECT;
+    }
+
+    private ControllerConstant() {
+    }
+
+    private static final String REDIRECT = "redirect:";
+}
