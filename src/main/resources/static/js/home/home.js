6fd11ed0193dcc15e2a44331fc6e843f95c2e963
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-18 00:51:25 +0700

    Add rajaongkir API to fetch on select2

--- src/main/resources/static/js/home/home.js
+++ src/main/resources/static/js/home/home.js
@@ -2,18 +2,98 @@ const $educationLevel = $('#education-level-select');
 const $province = $('#province-select');
 const $area = $('#area-select');
 
-const defaultConfigEducationLevel = {
-  placeholder: 'Select an option',
-};
-const defaultConfigProvince = {
-  placeholder: 'Select an option',
-};
-const defaultConfigArea = {
-  placeholder: 'Select an option',
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+const model = {
+  selected_province_id: null,
 };
 
-document.addEventListener('DOMContentLoaded', function() {
+const defaultConfigEducationLevel = {
+  language: 'id',
+  placeholder: 'Pilih salah satu',
+};
+
+const defaultConfigProvince = {
+  ajax: {
+    url: corsProxy + 'https://api.rajaongkir.com/starter/province',
+    delay: 500,
+    headers: {
+      key: 'c28f1db1a7340c866d915387d5302123',
+    },
+    data: "",
+    dataType: 'json',
+    processResults: response => {
+      const { results } = response.rajaongkir;
+      const province = $.map(results, (obj) => {
+        return {
+          id: Number(obj.province_id),
+          text: obj.province,
+        }
+      });
+      return {
+        // param results is required to parse obj to select2
+        results: province,
+      }
+    },
+  },
+  language: 'id',
+  placeholder: 'Pilih salah satu',
+  width: '25%',
+};
+const defaultConfigArea = {
+  ajax: {
+    url: corsProxy + 'https://api.rajaongkir.com/starter/city',
+    delay: 500,
+    headers: {
+      key: 'c28f1db1a7340c866d915387d5302123',
+    },
+    data: () => {
+      if (model.selected_province_id) {
+        return {
+          province: model.selected_province_id,
+        };
+      }
+      return '';
+    },
+    dataType: 'json',
+    processResults: response => {
+      const { results } = response.rajaongkir;
+      const city = $.map(results, (obj) => {
+        return {
+          id: Number(obj.city_id),
+          text: obj.city_name,
+        }
+      });
+      return {
+        // param results is required to parse obj to select2
+        results: city,
+      }
+    },
+  },
+  data: {id: null, text: null},
+  disabled: 'true',
+  language: 'id',
+  placeholder: 'Pilih provinsi dahulu',
+  width: '25%',
+};
+
+function initSelect2() {
   $educationLevel.select2(defaultConfigEducationLevel);
   $province.select2(defaultConfigProvince);
   $area.select2(defaultConfigArea);
+}
+
+function resetAreaSelect() {
+  $area.val(null).trigger('change');
+  $area.select2(defaultConfigArea);
+  $area.prop('disabled', false);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initSelect2();
+  $province.on('select2:select', (e) => {
+    const { data } = e.params;
+    model.selected_province_id = data.id;
+    resetAreaSelect();
+  });
 });
