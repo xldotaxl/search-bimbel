bbd14d4f4dc96d0257927c5092c9dd10e3ee9d09
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-04-18 12:44:42 +0700

    Add rest routes for province

--- src/main/java/it/aldi/app/controller/Routes.java
+++ src/main/java/it/aldi/app/controller/Routes.java
@@ -4,9 +4,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Routes {
+
+    // MVC
     public static final String INDEX = "/";
     public static final String LOGOUT = "/logout";
     public static final String REGISTER = "/register";
     public static final String SEARCH = "/search";
     public static final String SIGNIN = "/signin";
+
+    // REST
+    public static final String API = "/api";
+    public static final String PROVINCE = "/province";
+    public static final String API_PROVINCE = API + PROVINCE;
 }
