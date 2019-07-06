e14db363dd73f037f61ae1cf4e086c030c606d2f
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-07-06 14:14:38 +0700

    Add id, username, and subjects to TutorDto

--- src/main/java/it/aldi/app/controller/rest/dto/response/TutorDto.java
+++ src/main/java/it/aldi/app/controller/rest/dto/response/TutorDto.java
@@ -1,18 +1,29 @@
 package it.aldi.app.controller.rest.dto.response;
 
+import it.aldi.app.domain.BimbelUser;
 import lombok.Value;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Value
 public class TutorDto {
+    private Long id;
     private String name;
     private String email;
+    private String username;
+    private List<String> subjects;
 
-    private TutorDto(String name, String email) {
-        this.name = name;
-        this.email = email;
+    public TutorDto(BimbelUser bimbelUser) {
+        id = bimbelUser.getId();
+        name = bimbelUser.getName();
+        email = bimbelUser.getEmail();
+        username = bimbelUser.getEmail();
+        // TODO: get subject from entity relation
+        subjects = Arrays.asList("Matematika", "Fisika");
     }
 
-    public static TutorDto valueOf(String name, String email) {
-        return new TutorDto(name, email);
+    public static TutorDto valueOf(BimbelUser bimbelUser) {
+        return new TutorDto(bimbelUser);
     }
 }
