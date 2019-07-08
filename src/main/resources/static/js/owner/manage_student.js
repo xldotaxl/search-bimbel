c9420ad6163b344d22f241a27ba3e61569f0444d
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-07-09 00:41:09 +0700

    Add manage_student page, css, js

---
+++ src/main/resources/static/js/owner/manage_student.js
@@ -0,0 +1,36 @@
+const $tableTutor = $('#table-tutor');
+
+function getActionButton(id) {
+  return `<a href="#">View</a> | <a href="#">Delete</a>`;
+}
+
+const dataTableProp = {
+  ajax: {
+    dataSrc: json => {
+      json.recordsTotal = json.length;
+      json.recordsFiltered = json.length;
+      return json;
+    },
+    url: `${tutorsApi}/${organizationIds}`,
+  },
+  columns: [
+    {data: 'name'},
+    {data: 'username'},
+    {data: 'email'},
+    {data: 'subjects'},
+    {
+      data: 'id',
+      render: (data) => {
+        return getActionButton(data);
+      },
+    },
+  ],
+  dom: 'ftipr',
+  processing: true,
+  searching: false,
+  serverSide: true,
+};
+
+$(document).ready(() => {
+  $tableTutor.DataTable(dataTableProp);
+});
